d0bc35d7c0d5a715876881a345a0c9868c238ad7
SergeyT <tolstikovsn@gmail.com>
2023-07-31 22:16:25 +0500

    fix eslint errors

--- src/scripts/calculations/monofocal.js
+++ src/scripts/calculations/monofocal.js
@@ -1,27 +1,26 @@
-import acuvueMonofocal from '../databases/acuvueMonofocal.js'
-import { checkEmptyOculusData, normalizedToOpticFormat } from './utils.js'
+import acuvueMonofocal from '../databases/acuvueMonofocal.js';
+import { checkEmptyOculusData, normalizedToOpticFormat } from './utils.js';
 
 const monofocalCalculate = (oculus) => {
-    if (oculus === null) return oculus;
-    const {sph} = oculus;
-    if ((sph > -4 && sph < 4)) return normalizedToOpticFormat(sph);
-    try {
-    const res = acuvueMonofocal[normalizedToOpticFormat(sph)].result.sph    
-    return res
-    } catch (err) {
-        return 'Указанные диоптрии вне диапазона'
-    }
-}
+  if (oculus === null) return oculus;
+  const { sph } = oculus;
+  if ((sph > -4 && sph < 4)) return normalizedToOpticFormat(sph);
+  try {
+    return acuvueMonofocal[normalizedToOpticFormat(sph)].result.sph;
+  } catch (err) {
+    return 'Указанные диоптрии вне диапазона';
+  }
+};
 
 const monofocal = (data) => {
-    const {od, os} = checkEmptyOculusData(data);
-    return {
-        od: {
-            sph:monofocalCalculate(od),
-        },
-        os: {
-            sph:monofocalCalculate(os),
-        } 
-    }
-}
+  const { od, os } = checkEmptyOculusData(data);
+  return {
+    od: {
+      sph: monofocalCalculate(od),
+    },
+    os: {
+      sph: monofocalCalculate(os),
+    },
+  };
+};
 export default monofocal;
